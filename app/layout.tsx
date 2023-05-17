e0f4cca62e824743037c1e3ca2fb041cb24e8e9f
HJ S. Miranda <hmiranda@moylanmnl.net>
2023-05-17 09:58:55 +0800

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,25 @@
+import { Nunito } from 'next/font/google'
 import './globals.css'
+import Navbar from './components/Navbar/Navbar'
+import ClientOnly from './components/ClientOnly'
+import RegisterModal from './components/Modal/RegisterModal'
+import ToasterProvider from './providers/ToasterProvide'
+
+export const metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb clone'
+}
+
+const font = Nunito({
+  subsets: ['latin'],
+})
+
+//Get rid of the console error of toLowerCase
+const someVar = undefined;
+const str = someVar || ''; // 👉️ ""
+const result = str.toLowerCase();
+console.log(result); // 👉️ ""
+
 
 export default function RootLayout({
   children,
@@ -12,7 +33,14 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className={font.className}>
+      <ClientOnly>
+        <ToasterProvider />
+        <RegisterModal />
+        <Navbar />
+      </ClientOnly>
+        {children}
+      </body>
     </html>
   )
 }
